96606dbfcff68647f7c609a8ae18666e61a08162
BorneO <savelis1908@gmail.com>
2025-12-08 18:41:39 +0300

    Task1

---
+++ Tyuiu.YarkovSD.Sprint6.Task1.V12.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.YarkovSD.Sprint6.Task1.V28.Lib
+{
+    public class DataService : ISprint6Task1V28
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] a = new double[stopValue - startValue + 1];
+            int s = 0;
+
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                a[s] = Math.Round(Math.Sin(i) + ((Math.Cos(i) + 1) / (2 - i)) + 2 * i, 2);
+                s++;
+            }
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.YarkovSD.Sprint6.Task1.V12/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.YarkovSD.Sprint6.Task1.V12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
