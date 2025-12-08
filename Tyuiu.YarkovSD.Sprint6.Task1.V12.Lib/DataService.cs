b4f08118edf4d663a21249bdadac5e07bfc1e7d5
BorneO <savelis1908@gmail.com>
2025-12-08 19:07:22 +0300

    Task1

--- Tyuiu.YarkovSD.Sprint6.Task1.V12.Lib/DataService.cs
+++ Tyuiu.YarkovSD.Sprint6.Task1.V12.Lib/DataService.cs
@@ -1,4 +1,4 @@
-﻿using System.Net.Http.Headers;
+﻿using System;
 using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.YarkovSD.Sprint6.Task1.V12.Lib
@@ -7,15 +7,31 @@ namespace Tyuiu.YarkovSD.Sprint6.Task1.V12.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] a = new double[stopValue - startValue + 1];
-            int s = 0;
+            int length = stopValue - startValue + 1;
+            double[] resultArray = new double[length];
 
-            for (int i = startValue; i <= stopValue; i++)
+            for (int i = 0, x = startValue; x <= stopValue; i++, x++)
             {
-                a[s] = Math.Round(Math.Sin(i) + ((Math.Cos(i) + 1) / (2 - i)) + 2 * i, 2);
-                s++;
+                // Проверка деления на ноль
+                if (Math.Abs(2 - x) < 0.0000001) // Учет погрешности double
+                {
+                    resultArray[i] = 0; // При делении на ноль возвращаем 0
+                }
+                else
+                {
+                    // Вычисляем функцию
+                    double value = CalculateFunction(x);
+                    resultArray[i] = Math.Round(value, 2);
+                }
             }
-            return a;
+
+            return resultArray;
+        }
+
+        private double CalculateFunction(double x)
+        {
+            // F(x) = sin(x) + (cos(x) + 1) / (2 - x) + 2x
+            return Math.Sin(x) + (Math.Cos(x) + 1) / (2 - x) + 2 * x;
         }
     }
 }
