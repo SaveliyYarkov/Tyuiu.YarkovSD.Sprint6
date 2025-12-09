25a5fea77f52afb85820224695207a23501c5706
BorneO <savelis1908@gmail.com>
2025-12-09 12:32:47 +0300

    Task3

---
+++ Tyuiu.YarkovSD.Sprint6.Task3.V23.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.YarkovSD.Sprint6.Task3.V23.Lib
+{
+    public class DataService : ISprint6Task3V23
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            // Создаем массив строк для удобства сортировки
+            int[][] rowsArray = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                rowsArray[i] = new int[cols];
+                for (int j = 0; j < cols; j++)
+                {
+                    rowsArray[i][j] = matrix[i, j];
+                }
+            }
+
+            // Сортируем массив строк по второму элементу (столбцу с индексом 1)
+            Array.Sort(rowsArray, (row1, row2) => row1[1].CompareTo(row2[1]));
+
+            // Преобразуем обратно в двумерный массив
+            int[,] result = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = rowsArray[i][j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.YarkovSD.Sprint6.Task3.V23/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.YarkovSD.Sprint6.Task3.V23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
