8fa4b1aa205ecf2088352ec77a14a180e790de16
BorneO <savelis1908@gmail.com>
2025-12-09 13:11:02 +0300

    Task5

---
+++ Tyuiu.YarkovSD.Sprint6.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.YarkovSD.Sprint6.Task5.V7.Lib
+{
+    public class DataService : ISprint6Task5V7
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            // Читаем все строки из файла
+            string[] lines = File.ReadAllLines(path);
+
+            // Преобразуем строки в числа
+            double[] numbers = new double[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                numbers[i] = Convert.ToDouble(lines[i]);
+            }
+
+            // Фильтруем: оставляем только числа > 5
+            double[] result = numbers.Where(val => val > 5).ToArray();
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.YarkovSD.Sprint6.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.YarkovSD.Sprint6.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
