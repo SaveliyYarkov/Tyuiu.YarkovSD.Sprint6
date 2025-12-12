88c696ca2826aeda2681e9e6300ee33ea86ef1db
BorneO <savelis1908@gmail.com>
2025-12-12 19:53:13 +0300

    Task7

--- Tyuiu.YarkovSD.Sprint6.Task7.V10.Lib/DataService.cs
+++ Tyuiu.YarkovSD.Sprint6.Task7.V10.Lib/DataService.cs
@@ -1,5 +1,6 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint6;
 using System.IO;
+using System.Linq;
 
 namespace Tyuiu.YarkovSD.Sprint6.Task7.V10.Lib
 {
@@ -7,42 +8,45 @@ namespace Tyuiu.YarkovSD.Sprint6.Task7.V10.Lib
     {
         public int[,] GetMatrix(string path)
         {
-            string[] lines = File.ReadAllLines(path);
+            string[] lines = File.ReadAllLines(path)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .ToArray();
+
+            if (lines.Length < 5)
+                throw new ArgumentException($"Требуется минимум 5 строк. Найдено: {lines.Length}");
 
             int rows = lines.Length;
             string[] firstLine = lines[0].Split(';');
             int cols = firstLine.Length;
 
             int[,] matrix = new int[rows, cols];
+            int[,] result = new int[rows, cols];
 
-            // Заполняем матрицу данными из файла
             for (int i = 0; i < rows; i++)
             {
                 string[] values = lines[i].Split(';');
 
                 for (int j = 0; j < cols; j++)
                 {
-                    matrix[i, j] = int.Parse(values[j]);
+                    if (int.TryParse(values[j]?.Trim() ?? "0", out int value))
+                    {
+                        matrix[i, j] = value;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
                 }
             }
 
-            int[,] result = new int[rows, cols];
-
+            // Обрабатываем пятую строку
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    if (i == 4)
+                    if (i == 4 && matrix[i, j] >= 5 && matrix[i, j] <= 10)
                     {
-                        int value = matrix[i, j];
-                        if (value >= 5 && value <= 10)
-                        {
-                            result[i, j] = 0;
-                        }
-                        else
-                        {
-                            result[i, j] = matrix[i, j];
-                        }
+                        result[i, j] = 0;
                     }
                     else
                     {
